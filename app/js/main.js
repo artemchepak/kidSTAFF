655165f49a2039e0fce5d89a0b315798e2794c46
Artem Chepak <artemchepakwebdeveloper@users.noreply.github.com>
2021-02-26 21:40:36 +0200

    feat: add banner slider and categories section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,71 @@
+$(function() {
+    //banner slider
+    $('.banner-section__slider').slick({
+        infinite: true,
+        dots: true,
+        autoplay: true,
+        arrows: false
+    });
+
+    //search tabs
+    $('.tab').on('click', function(e) {
+        e.preventDefault();
+
+        $($(this).siblings()).removeClass('tab--active');
+        $($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
+
+        $(this).addClass('tab--active');
+        $($(this).attr('href')).addClass('tabs-content--active');
+
+    });
+
+    //favourite icon    
+    $('.product-item__favourite').on('click', function() {
+        $(this).toggleClass('product-item__favourite--active');
+    });
+
+    //product-slider
+    $('.product-slider').slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/arrow-black-left.svg" alt="left arrow"></button>',
+        nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-black-right.svg" alt="right arrow"></button>'
+    })
+
+    // aside
+
+    $('.filter-style').styler();
+
+    $('.filter__item-drop, .filter__extra').on('click', function() {
+        $(this).toggleClass('filter__item-drop--active');
+        $(this).next().slideToggle('200');
+    });
+
+    $(".js-range-slider").ionRangeSlider();
+
+    $('.catalog__filter-btngrid').on('click', function() {
+        $(this).addClass('catalog__filter-button--active');
+        $('.catalog__filter-btnline').removeClass('catalog__filter-button--active');
+        $('.product-item__wrapper').removeClass('product-item__wrapper--list');
+    });
+
+    $('.catalog__filter-btnline').on('click', function() {
+        $(this).addClass('catalog__filter-button--active');
+        $('.catalog__filter-btngrid').removeClass('catalog__filter-button--active');
+        $('.product-item__wrapper').addClass('product-item__wrapper--list');
+    });
+
+    $("#rate-yo").rateYo({
+        normalFill: "#C4C4C4",
+        ratedFill: "#1C62CD",
+        spacing: "7px",
+    });
+
+});
+
+//index script
 const left = document.querySelector('.left')
 const right = document.querySelector('.right')
 const container = document.querySelector('.index__container')
